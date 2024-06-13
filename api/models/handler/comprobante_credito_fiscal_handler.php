6f9c896e63a5b2220ec2d427bd10099013905da3
sochiiii <20220018@ricaldone.edu.sv>
2024-06-13 12:10:33 -0600

    api/models

--- api/models/handler/comprobante_credito_fiscal_handler.php
+++ api/models/handler/comprobante_credito_fiscal_handler.php
@@ -49,82 +49,62 @@ class ComprobanteCreditoHandler
 
     public function readOne()
     {
-        $sql = 'SELECT id_producto, nombre_producto, descripcion_producto, precio_producto, existencias_producto, imagen_producto, id_categoria, estado_producto
-                FROM producto
-                WHERE id_producto = ?';
-        $params = array($this->id);
-        return Database::getRow($sql, $params);
-    }
-
-    public function readFilename()
-    {
-        $sql = 'SELECT imagen_producto
-                FROM producto
-                WHERE id_producto = ?';
+        $sql = 'SELECT id_comprobante, id_cliente, id_servicio, nit_credito_fiscal, nombre_credito_fiscal, ncr_credito_fiscal, gito_credito_fiscal, direccion_credito_fiscal, email_credito_fiscal, telefono_credito_fiscal, dui_credito_fiscal
+                FROM tb_comprobante_credito_fiscal
+                WHERE id_comprobante = ?';
         $params = array($this->id);
         return Database::getRow($sql, $params);
     }
 
     public function updateRow()
     {
-        $sql = 'UPDATE producto
-                SET imagen_producto = ?, nombre_producto = ?, descripcion_producto = ?, precio_producto = ?, estado_producto = ?, id_categoria = ?
-                WHERE id_producto = ?';
-        $params = array($this->imagen, $this->nombre, $this->descripcion, $this->precio, $this->estado, $this->categoria, $this->id);
+        $sql = 'UPDATE tb_comprobante_credito_fiscal
+                SET nit_credito_fiscal = ?, nombre_credito_fiscal = ?, nrc_credito_fiscal = ?, giro_credito_fiscal = ?, direccion_credito_fiscal = ?, email_credito_fiscal = ?, telefono_credito_fiscal = ?, dui_credito_fiscal = ?
+                WHERE id_comprobante = ?';
+        $params = array($this->nit, $this->nombre, $this->nrc, $this->giro, $this->direccion, $this->email, $this->direccion, $this->id);
         return Database::executeRow($sql, $params);
     }
 
     public function deleteRow()
     {
-        $sql = 'DELETE FROM producto
-                WHERE id_producto = ?';
+        $sql = 'DELETE FROM tb_comprobante_credito_fiscal
+                WHERE id_comprobante = ?';
         $params = array($this->id);
         return Database::executeRow($sql, $params);
     }
 
-    public function readProductosCategoria()
-    {
-        $sql = 'SELECT id_producto, imagen_producto, nombre_producto, descripcion_producto, precio_producto, existencias_producto
-                FROM producto
-                INNER JOIN categoria USING(id_categoria)
-                WHERE id_categoria = ? AND estado_producto = true
-                ORDER BY nombre_producto';
-        $params = array($this->categoria);
-        return Database::getRows($sql, $params);
-    }
+    // /*
+    // *   Métodos para generar gráficos.
+    // */
+    // public function cantidadProductosCategoria()
+    // {
+    //     $sql = 'SELECT nombre_categoria, COUNT(id_producto) cantidad
+    //             FROM producto
+    //             INNER JOIN categoria USING(id_categoria)
+    //             GROUP BY nombre_categoria ORDER BY cantidad DESC LIMIT 5';
+    //     return Database::getRows($sql);
+    // }
 
-    /*
-    *   Métodos para generar gráficos.
-    */
-    public function cantidadProductosCategoria()
-    {
-        $sql = 'SELECT nombre_categoria, COUNT(id_producto) cantidad
-                FROM producto
-                INNER JOIN categoria USING(id_categoria)
-                GROUP BY nombre_categoria ORDER BY cantidad DESC LIMIT 5';
-        return Database::getRows($sql);
-    }
+    // public function porcentajeProductosCategoria()
+    // {
+    //     $sql = 'SELECT nombre_categoria, ROUND((COUNT(id_producto) * 100.0 / (SELECT COUNT(id_producto) FROM producto)), 2) porcentaje
+    //             FROM producto
+    //             INNER JOIN categoria USING(id_categoria)
+    //             GROUP BY nombre_categoria ORDER BY porcentaje DESC';
+    //     return Database::getRows($sql);
+    // }
 
-    public function porcentajeProductosCategoria()
-    {
-        $sql = 'SELECT nombre_categoria, ROUND((COUNT(id_producto) * 100.0 / (SELECT COUNT(id_producto) FROM producto)), 2) porcentaje
-                FROM producto
-                INNER JOIN categoria USING(id_categoria)
-                GROUP BY nombre_categoria ORDER BY porcentaje DESC';
-        return Database::getRows($sql);
-    }
-
-    /*
-    *   Métodos para generar reportes.
-    */
-    public function productosCategoria()
-    {
-        $sql = 'SELECT nombre_producto, precio_producto, estado_producto
-                FROM producto
-                INNER JOIN categoria USING(id_categoria)
-                WHERE id_categoria = ?
-                ORDER BY nombre_producto';
-        $params = array($this->categoria);
-        return Database::getRows($sql, $params);
-    }
+    // /*
+    // *   Métodos para generar reportes.
+    // */
+    // public function productosCategoria()
+    // {
+    //     $sql = 'SELECT nombre_producto, precio_producto, estado_producto
+    //             FROM producto
+    //             INNER JOIN categoria USING(id_categoria)
+    //             WHERE id_categoria = ?
+    //             ORDER BY nombre_producto';
+    //     $params = array($this->categoria);
+    //     return Database::getRows($sql, $params);
+    // }
 }
