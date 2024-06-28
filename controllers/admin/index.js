8b972298afb3b5bb0fc5257657a44f6e748422fe
sochiiii <20220018@ricaldone.edu.sv>
2024-06-28 10:02:01 -0600

    mejora de primer uso y login muñeca uwu

--- controllers/admin/index.js
+++ controllers/admin/index.js
@@ -1,7 +1,7 @@
 // Constante para establecer el formulario de registro del primer usuario.
-// const SIGNUP_FORM = document.getElementById('signupForm');
 // Constante para establecer el formulario de inicio de sesión.
 const LOGIN_FORM = document.getElementById('loginForm');
+const SIGNUP_FORM = document.getElementById('signupForm');
 
 const LOGIN_CONTENT = document.getElementById("loginContent");
 const SIGNUP_CONTENT = document.getElementById("signUpContent");
@@ -17,7 +17,6 @@ document.addEventListener('DOMContentLoaded', async () => {
         location.href = 'inicio.html';
     } else if (DATA.status) {
         // Se establece el título del contenido principal.
-       // MAIN_TITLE.textContent = 'Iniciar sesión';
         // Se muestra el formulario para iniciar sesión.
         LOGIN_CONTENT.classList.add('show');
         sweetAlert(4, DATA.message, true);
@@ -30,20 +29,20 @@ document.addEventListener('DOMContentLoaded', async () => {
 });
 
 // Método del evento para cuando se envía el formulario de registro del primer usuario.
-// SIGNUP_FORM.addEventListener('submit', async (event) => {
-//     // Se evita recargar la página web después de enviar el formulario.
-//     event.preventDefault();
-//     // Constante tipo objeto con los datos del formulario.
-//     const FORM = new FormData(SIGNUP_FORM);
-//     // Petición para registrar el primer usuario del sitio privado.
-//     const DATA = await fetchData(USER_API, 'signUp', FORM);
-//     // Se comprueba si la respuesta es satisfactoria, de lo contrario se muestra un mensaje con la excepción.
-//     if (DATA.status) {
-//         sweetAlert(1, DATA.message, true, 'index.html');
-//     } else {
-//         sweetAlert(2, DATA.error, false);
-//     }
-// });
+SIGNUP_FORM.addEventListener('submit', async (event) => {
+    // Se evita recargar la página web después de enviar el formulario.
+    event.preventDefault();
+    // Constante tipo objeto con los datos del formulario.
+    const FORM = new FormData(SIGNUP_FORM);
+    // Petición para registrar el primer usuario del sitio privado.
+    const DATA = await fetchData(USER_API, 'signUp', FORM);
+    // Se comprueba si la respuesta es satisfactoria, de lo contrario se muestra un mensaje con la excepción.
+    if (DATA.status) {
+        sweetAlert(1, DATA.message, true, 'index.html');
+    } else {
+        sweetAlert(2, DATA.error, false);
+    }
+});
 
 // Método del evento para cuando se envía el formulario de inicio de sesión.
 LOGIN_FORM.addEventListener('submit', async (event) => {
@@ -55,7 +54,6 @@ LOGIN_FORM.addEventListener('submit', async (event) => {
     const DATA = await fetchData(USER_API, 'logIn', FORM);
     // Se comprueba si la respuesta es satisfactoria, de lo contrario se muestra un mensaje con la excepción.
     if (DATA.status) {
-        alert(DATA)
         console.log(DATA);
         sweetAlert(1, DATA.message, true, 'inicio.html');
     } else {
