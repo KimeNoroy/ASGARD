9b6821cc767db2700452a6cd23369f0e7b0747c3
KimeNoroy <villegasfernando021@gmail.com>
2024-07-03 19:44:18 -0600

    prueba

---
+++ controllers/admin/empleado.js
@@ -0,0 +1,173 @@
+
+// Constantes para completar las rutas de la API.
+const EMPLEADO_API = 'services/admin/empleados.php';
+//const CATEGORIA_API = 'services/admin/categoria.php';
+// Constante para establecer el formulario de buscar.
+//const SEARCH_FORM = document.getElementById('searchForm');
+// Constantes para establecer el contenido de la tabla.
+const TABLE_BODY = document.getElementById('tableBodyEmpleados'),
+    ROWS_FOUND = document.getElementById('rowsFound');
+// Constantes para establecer los elementos del componente Modal.
+const SAVE_MODAL = new bootstrap.Modal('#crearModal');
+   // MODAL_TITLE = document.getElementById('modalTitle');
+// Constantes para establecer los elementos del formulario de guardar.
+const SAVE_FORM = document.getElementById('saveForm'),
+    ID_EMPLEADO = document.getElementById('id_empleado'),
+    NOMBRE_EMPLEADO = document.getElementById('nombres_empleado'),
+    APELLIDO_EMPLEADO = document.getElementById('apellidos_empleado'),
+    DUI_EMPLEADO = document.getElementById('dui_empleado'),
+    EMAIL_EMPLEADO = document.getElementById('email_empleado'),
+    PASSWORD_EMPLEADO = document.getElementById('contrasena');
+
+// Método del evento para cuando el documento ha cargado.
+document.addEventListener('DOMContentLoaded', () => {
+    // Llamada a la función para mostrar el encabezado y pie del documento.
+    //loadTemplate();
+    // Se establece el título del contenido principal.
+    //MAIN_TITLE.textContent = 'Gestionar clientes';
+    // Llamada a la función para llenar la tabla con los registros existentes.
+    fillTable();
+});
+
+
+// Método del evento para cuando se envía el formulario de guardar.
+SAVE_FORM.addEventListener('submit', async (event) => {
+    // Se evita recargar la página web después de enviar el formulario.
+    event.preventDefault();
+    // Se verifica la acción a realizar.
+    (ID_EMPLEADO.value) ? action = 'updateRow' : action = 'createRow';
+    // Constante tipo objeto con los datos del formulario.
+    const FORM = new FormData(SAVE_FORM);
+    // Petición para guardar los datos del formulario.
+    const DATA = await fetchData(EMPLEADO_API, action, FORM);
+    // Se comprueba si la respuesta es satisfactoria, de lo contrario se muestra un mensaje con la excepción.
+    if (DATA.status) {
+        // Se cierra la caja de diálogo.
+        SAVE_MODAL.hide();
+        // Se muestra un mensaje de éxito.
+        sweetAlert(1, DATA.message, true);
+        // Se carga nuevamente la tabla para visualizar los cambios.
+        fillTable();
+    } else {
+        sweetAlert(2, DATA.error, false);
+    }
+});
+
+/*
+*   Función asíncrona para llenar la tabla con los registros disponibles.
+*   Parámetros: form (objeto opcional con los datos de búsqueda).
+*   Retorno: ninguno.
+*/
+const fillTable = async (form = null) => {
+    // Se inicializa el contenido de la tabla.
+    ROWS_FOUND.textContent = '';
+    TABLE_BODY.innerHTML = '';
+    // Se verifica la acción a realizar.
+    (form) ? action = 'searchRows' : action = 'readAll';
+    // Petición para obtener los registros disponibles.
+    const DATA = await fetchData(CLIENTE_API, action, form);
+    // Se comprueba si la respuesta es satisfactoria, de lo contrario se muestra un mensaje con la excepción.
+    if (DATA.status) {
+        // Se recorre el conjunto de registros (dataset) fila por fila a través del objeto row.
+        DATA.dataset.forEach(row => {
+            // Se establece un icono para el estado del producto.
+            //(row.estado_producto) ? icon = 'bi bi-eye-fill' : icon = 'bi bi-eye-slash-fill';
+            // Se crean y concatenan las filas de la tabla con los datos de cada registro.
+            TABLE_BODY.innerHTML += `
+                <tr>
+                    <td>${row.nombres_empleado}</td>
+                    <td>${row.apellidos_empleado}</td>
+                    <td>${row.dui_empleado}</td>
+                    <td>
+                        <button type="button" class="btn btn-info" onclick="openUpdate(${row.id_empleado})">
+                            <i class="bi bi-pencil-fill"></i>
+                        </button>
+                        <button type="button" class="btn btn-danger" onclick="openDelete(${row.id_empleado})">
+                            <i class="bi bi-trash-fill"></i>
+                        </button>
+                    </td>
+                </tr>
+            `;
+        });
+        // Se muestra un mensaje de acuerdo con el resultado.
+        ROWS_FOUND.textContent = DATA.message;
+    } else {
+        sweetAlert(4, DATA.error, true);
+    }
+}
+
+/*
+*   Función para preparar el formulario al momento de insertar un registro.
+*   Parámetros: ninguno.
+*   Retorno: ninguno.
+*/
+const openCreate = () => {
+    // Se muestra la caja de diálogo con su título.
+    SAVE_MODAL.show();
+    //MODAL_TITLE.textContent = 'Crear empleado';
+    // Se prepara el formulario.
+    SAVE_FORM.reset();
+    //EXISTENCIAS_PRODUCTO.disabled = false;
+    //fillSelect(CATEGORIA_API, 'readAll', 'categoriaProducto');
+}
+
+/*
+*   Función asíncrona para preparar el formulario al momento de actualizar un registro.
+*   Parámetros: id (identificador del registro seleccionado).
+*   Retorno: ninguno.
+*/
+const openUpdate = async (id) => {
+    // Se define un objeto con los datos del registro seleccionado.
+    const FORM = new FormData();
+    FORM.append('id_empleado', id);
+    // Petición para obtener los datos del registro solicitado.
+    const DATA = await fetchData(EMPLEADO_API, 'readOne', FORM);
+    // Se comprueba si la respuesta es satisfactoria, de lo contrario se muestra un mensaje con la excepción.
+    if (DATA.status) {
+        // Se muestra la caja de diálogo con su título.
+        SAVE_MODAL.show();
+        //MODAL_TITLE.textContent = 'Actualizar cliente';
+        // Se prepara el formulario.
+        SAVE_FORM.reset();
+        //EXISTENCIAS_PRODUCTO.disabled = true;
+        // Se inicializan los campos con los datos.
+        const ROW = DATA.dataset;
+        ID_EMPLEADO.value = ROW.id_empleado;
+        NOMBRE_EMPLEADO.value = ROW.nombres_empleado;
+        APELLIDO_EMPLEADO.value = ROW.apellidos_empleado;
+        DUI_EMPLEADO.value = ROW.dui_empleado;
+        EMAIL_EMPLEADO.value = ROW.email_empleado;
+        PASSWORD_EMPLEADO.value = ROW.contrasena;
+        
+        //fillSelect(CATEGORIA_API, 'readAll', 'categoriaProducto', ROW.id_categoria);
+    } else {
+        sweetAlert(2, DATA.error, false);
+    }
+}
+
+/*
+*   Función asíncrona para eliminar un registro.
+*   Parámetros: id (identificador del registro seleccionado).
+*   Retorno: ninguno.
+*/
+const openDelete = async (id) => {
+    // Llamada a la función para mostrar un mensaje de confirmación, capturando la respuesta en una constante.
+    const RESPONSE = await confirmAction('¿Desea eliminar el empleado de forma permanente?');
+    // Se verifica la respuesta del mensaje.
+    if (RESPONSE) {
+        // Se define una constante tipo objeto con los datos del registro seleccionado.
+        const FORM = new FormData();
+        FORM.append('id_empleado', id);
+        // Petición para eliminar el registro seleccionado.
+        const DATA = await fetchData(EMPLEADO_API, 'deleteRow', FORM);
+        // Se comprueba si la respuesta es satisfactoria, de lo contrario se muestra un mensaje con la excepción.
+        if (DATA.status) {
+            // Se muestra un mensaje de éxito.
+            await sweetAlert(1, DATA.message, true);
+            // Se carga nuevamente la tabla para visualizar los cambios.
+            fillTable();
+        } else {
+            sweetAlert(2, DATA.error, false);
+        }
+    }
+}
