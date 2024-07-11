038cdaba9d51b0c0ae15ed874f6f2dbdc51bb765
Roberto07E <136492762+Roberto07E@users.noreply.github.com>
2024-07-10 23:03:08 -0300

    Update empleado.js

--- controllers/admin/empleado.js
+++ controllers/admin/empleado.js
@@ -1,44 +1,52 @@
 // Constantes para completar las rutas de la API.
-const EMPLEADO_API = 'services/admin/empleado.php';
-//const CATEGORIA_API = 'services/admin/categoria.php';
+const ADMINISTADOR_API = 'services/admin/administrador.php';
 // Constante para establecer el formulario de buscar.
-//const SEARCH_FORM = document.getElementById('searchForm');
+const SEARCH_FORM = document.getElementById('searchForm');
 // Constantes para establecer el contenido de la tabla.
-const TABLE_BODY = document.getElementById('tableBodyEmpleados'),
+const TABLE_BODY = document.getElementById('tableBody'),
     ROWS_FOUND = document.getElementById('rowsFound');
 // Constantes para establecer los elementos del componente Modal.
-const SAVE_MODAL = new bootstrap.Modal('#crearModal');
-   // MODAL_TITLE = document.getElementById('modalTitle');
+const SAVE_MODAL = new bootstrap.Modal('#saveModal'),
+    MODAL_TITLE = document.getElementById('modalTitle');
 // Constantes para establecer los elementos del formulario de guardar.
 const SAVE_FORM = document.getElementById('saveForm'),
-    ID_EMPLEADO = document.getElementById('id_empleado'),
-    NOMBRE_EMPLEADO = document.getElementById('nombres_empleado'),
-    APELLIDO_EMPLEADO = document.getElementById('apellidos_empleado'),
-    DUI_EMPLEADO = document.getElementById('dui_empleado'),
-    EMAIL_EMPLEADO = document.getElementById('email_empleado'),
-    PASSWORD_EMPLEADO = document.getElementById('contrasena');
+    ID_ADMINISTRADOR = document.getElementById('idAdmin'),
+    NOMBRE_ADMINISTRADOR = document.getElementById('NAdmin'),
+    APELLIDO_ADMINISTRADOR = document.getElementById('ApAdmin'),
+    CORREO_ADMINISTRADOR = document.getElementById('CorreoAd'),
+    CONTRASEÑA_ADMINISTRADOR = document.getElementById('ContraAd'),
+    CONTRASEÑA_CONFIRMAR_ADMINISTRADOR = document.getElementById('confirmarClaveA');
+document.querySelector('title').textContent = 'Empleados';
 
 // Método del evento para cuando el documento ha cargado.
 document.addEventListener('DOMContentLoaded', () => {
     // Llamada a la función para mostrar el encabezado y pie del documento.
-    //loadTemplate();
+    loadTemplate();
     // Se establece el título del contenido principal.
-    //MAIN_TITLE.textContent = 'Gestionar clientes';
     // Llamada a la función para llenar la tabla con los registros existentes.
     fillTable();
 });
 
+// Método del evento para cuando se envía el formulario de buscar.
+SEARCH_FORM.addEventListener('submit', (event) => {
+    // Se evita recargar la página web después de enviar el formulario.
+    event.preventDefault();
+    // Constante tipo objeto con los datos del formulario.
+    const FORM = new FormData(SEARCH_FORM);
+    // Llamada a la función para llenar la tabla con los resultados de la búsqueda.
+    fillTable(FORM);
+});
 
 // Método del evento para cuando se envía el formulario de guardar.
 SAVE_FORM.addEventListener('submit', async (event) => {
     // Se evita recargar la página web después de enviar el formulario.
     event.preventDefault();
     // Se verifica la acción a realizar.
-    (ID_EMPLEADO.value) ? action = 'updateRow' : action = 'createRow';
+    (ID_ADMINISTRADOR.value) ? action = 'updateRow' : action = 'createTrabajadores';
     // Constante tipo objeto con los datos del formulario.
     const FORM = new FormData(SAVE_FORM);
     // Petición para guardar los datos del formulario.
-    const DATA = await fetchData(EMPLEADO_API, action, FORM);
+    const DATA = await fetchData(ADMINISTADOR_API, action, FORM);
     // Se comprueba si la respuesta es satisfactoria, de lo contrario se muestra un mensaje con la excepción.
     if (DATA.status) {
         // Se cierra la caja de diálogo.
@@ -64,24 +72,22 @@ const fillTable = async (form = null) => {
     // Se verifica la acción a realizar.
     (form) ? action = 'searchRows' : action = 'readAll';
     // Petición para obtener los registros disponibles.
-   const DATA = await fetchData(EMPLEADO_API, action, form);
+    const DATA = await fetchData(ADMINISTADOR_API, action, form);
     // Se comprueba si la respuesta es satisfactoria, de lo contrario se muestra un mensaje con la excepción.
     if (DATA.status) {
-        // Se recorre el conjunto de registros (dataset) fila por fila a través del objeto row.
+        // Se recorre el conjunto de registros fila por fila.
         DATA.dataset.forEach(row => {
-            // Se establece un icono para el estado del producto.
-            //(row.estado_producto) ? icon = 'bi bi-eye-fill' : icon = 'bi bi-eye-slash-fill';
             // Se crean y concatenan las filas de la tabla con los datos de cada registro.
             TABLE_BODY.innerHTML += `
                 <tr>
-                    <td>${row.nombres_empleado}</td>
-                    <td>${row.apellidos_empleado}</td>
-                    <td>${row.dui_empleado}</td>
+                    <td>${row.nombre_administrador}</td>
+                    <td>${row.apellido_administrador}</td>
+                    <td>${row.email_administrador}</td>
                     <td>
-                        <button type="button" class="btn btn-info" onclick="openUpdate(${row.id_empleado})">
+                        <button type="button" class="btn btn-info rounded me-2 mb-2 mb-sm-2" onclick="openUpdate(${row.id_administrador})">
                             <i class="bi bi-pencil-fill"></i>
                         </button>
-                        <button type="button" class="btn btn-danger" onclick="openDelete(${row.id_empleado})">
+                        <button type="button" class="btn btn-danger rounded me-2 mb-2 mb-sm-2 " onclick="openDelete(${row.id_administrador})">
                             <i class="bi bi-trash-fill"></i>
                         </button>
                     </td>
@@ -103,11 +109,12 @@ const fillTable = async (form = null) => {
 const openCreate = () => {
     // Se muestra la caja de diálogo con su título.
     SAVE_MODAL.show();
-    //MODAL_TITLE.textContent = 'Crear empleado';
+    MODAL_TITLE.textContent = 'Crear empleado';
     // Se prepara el formulario.
     SAVE_FORM.reset();
-    //EXISTENCIAS_PRODUCTO.disabled = false;
-    //fillSelect(CATEGORIA_API, 'readAll', 'categoriaProducto');
+    ALIAS_ADMINISTRADOR.disabled = false;
+    CLAVE_ADMINISTRADOR.disabled = false;
+    CONFIRMAR_CLAVE.disabled = false;
 }
 
 /*
@@ -116,29 +123,26 @@ const openCreate = () => {
 *   Retorno: ninguno.
 */
 const openUpdate = async (id) => {
-    // Se define un objeto con los datos del registro seleccionado.
+    // Se define una constante tipo objeto con los datos del registro seleccionado.
     const FORM = new FormData();
-    FORM.append('id_empleado', id);
+    FORM.append('idAdmin', id);
     // Petición para obtener los datos del registro solicitado.
-    const DATA = await fetchData(EMPLEADO_API, 'readOne', FORM);
+    const DATA = await fetchData(ADMINISTADOR_API, 'readOne', FORM);
     // Se comprueba si la respuesta es satisfactoria, de lo contrario se muestra un mensaje con la excepción.
     if (DATA.status) {
         // Se muestra la caja de diálogo con su título.
         SAVE_MODAL.show();
-        //MODAL_TITLE.textContent = 'Actualizar cliente';
+        MODAL_TITLE.textContent = 'Actualizar empleado';
         // Se prepara el formulario.
         SAVE_FORM.reset();
-        //EXISTENCIAS_PRODUCTO.disabled = true;
+        CONTRASEÑA_ADMINISTRADOR.disabled = true;
+        CONTRASEÑA_CONFIRMAR_ADMINISTRADOR.disabled = true;
         // Se inicializan los campos con los datos.
         const ROW = DATA.dataset;
-        ID_EMPLEADO.value = ROW.id_empleado;
-        NOMBRE_EMPLEADO.value = ROW.nombres_empleado;
-        APELLIDO_EMPLEADO.value = ROW.apellidos_empleado;
-        DUI_EMPLEADO.value = ROW.dui_empleado;
-        EMAIL_EMPLEADO.value = ROW.email_empleado;
-        PASSWORD_EMPLEADO.value = ROW.contrasena;
-        
-        //fillSelect(CATEGORIA_API, 'readAll', 'categoriaProducto', ROW.id_categoria);
+        ID_ADMINISTRADOR.value = ROW.id_administrador;
+        NOMBRE_ADMINISTRADOR.value = ROW.nombre_administrador;
+        APELLIDO_ADMINISTRADOR.value = ROW.apellido_administrador;
+        CORREO_ADMINISTRADOR.value = ROW.email_administrador;
     } else {
         sweetAlert(2, DATA.error, false);
     }
@@ -156,9 +160,9 @@ const openDelete = async (id) => {
     if (RESPONSE) {
         // Se define una constante tipo objeto con los datos del registro seleccionado.
         const FORM = new FormData();
-        FORM.append('id_empleado', id);
+        FORM.append('idAdmin', id);
         // Petición para eliminar el registro seleccionado.
-        const DATA = await fetchData(EMPLEADO_API, 'deleteRow', FORM);
+        const DATA = await fetchData(ADMINISTADOR_API, 'deleteRow', FORM);
         // Se comprueba si la respuesta es satisfactoria, de lo contrario se muestra un mensaje con la excepción.
         if (DATA.status) {
             // Se muestra un mensaje de éxito.
